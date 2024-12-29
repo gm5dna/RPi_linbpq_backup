df6c98366f72554cb8b363037849f469ad15c158
Stuart Henderson <stuart@craggiemore.com>
2024-12-29 19:58:44 +0000

    added bpq32.cfg to archive as well as some personal changed

--- RPi_linbpq_backup.sh
+++ RPi_linbpq_backup.sh
@@ -6,9 +6,10 @@
 # Daily BPQ backup from /opt/oarc/bpq/ to local network samba share
 # Each backup is date stamped and retained for a rolling 10 days
 #
-DATESHORT=`date +%Y%m%d`                                                        # Date string without dashes for filename                                                           # Source directory
-SRCDIR=/opt/oarc/bpq/                                                           # Source for the backup
-DESDIR=/mnt/smb_share/BPQbackup                                                 # Destination directory
+DATESHORT=`date +%Y%m%d`                                                        # Date string without dashes for filename
+SRCDIR=/opt/oarc/bpq/                                                           # Main application directory for the backup
+DESDIR=~/bpq-backup                                                             # Destination directory (ideally an SMB share)
+BPQCFG=/etc/bpq32.cfg                                                           # Location of bpq32.cfg on Hibbian repo
 FILE=bpq$DATESHORT.tar.gz                                                       # Backup file name
 REMOVEDATE=$(date --date="10 days ago" +%Y%m%d)                                 # throw away files older than 10 days
 REMOVEFILE=bpq$REMOVEDATE.tar.gz                                                # File to remove
@@ -16,8 +17,8 @@ REMOVEFILE=bpq$REMOVEDATE.tar.gz                                                
 # Let's start with the backup
 #
 sudo systemctl stop linbpq.service                                              # Stop BPQ service
-sudo tar -cpzf $DESDIR/$FILE $SRCDIR    >> /home/robin/job_result.log 2>&1      # Compressed backup and log file appended
-sudo rm $DESDIR/$REMOVEFILE > /dev/null 2>&1                                         # Remove old files
+sudo tar -cpzf $DESDIR/$FILE $SRCDIR $BPQCFG  >> $DESDIR/bpq-backup.log 2>&1    # Compressed backup and log file appended
+sudo rm $DESDIR/$REMOVEFILE > /dev/null 2>&1                                    # Remove old files
 sudo systemctl start linbpq.service                                             # Start BPQ service
 #
 # Job's a goodun :)
