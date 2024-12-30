a31cc242ee3cdb31a25b24a7cf884529f1e73c58
Stuart Henderson <stuart@craggiemore.com>
2024-12-30 10:46:07 +0000

    added an alternative version with better logging

--- RPi_linbpq_backup.sh
+++ RPi_linbpq_backup.sh
@@ -8,11 +8,11 @@
 #
 DATESHORT=`date +%Y%m%d`                                                        # Date string without dashes for filename
 SRCDIR=/opt/oarc/bpq/                                                           # Main application directory for the backup
+BPQCFG=/etc/bpq32.cfg                                                           # Location of bpq32.cfg (/etc on Hibbian repo)
 DESDIR=~/bpq-backup                                                             # Destination directory (ideally an SMB share)
-SYNCDIR=onedrive:/Backups/LinBPQ                                                # Rclone director
-BPQCFG=/etc/bpq32.cfg                                                           # Location of bpq32.cfg on Hibbian repo
+SYNCDIR=onedrive:/Backups/LinBPQ                                                # Rclone target directory
 FILE=bpq$DATESHORT.tar.gz                                                       # Backup file name
-REMOVEDATE=$(date --date="10 days ago" +%Y%m%d)                                 # throw away files older than 10 days
+REMOVEDATE=$(date --date="10 days ago" +%Y%m%d)                                 # Throw away files older than 10 days
 REMOVEFILE=bpq$REMOVEDATE.tar.gz                                                # File to remove
 #
 # Let's start with the backup
@@ -21,6 +21,6 @@ sudo systemctl stop linbpq.service                                              
 sudo tar -cpzf $DESDIR/$FILE $SRCDIR $BPQCFG  >> $DESDIR/bpq-backup.log 2>&1    # Compressed backup and log file appended
 sudo rm $DESDIR/$REMOVEFILE > /dev/null 2>&1                                    # Remove old files
 sudo systemctl start linbpq.service                                             # Start BPQ service
-rclone sync $DESDIR onedrive:/Backups/LinBPQ --progress                         # Rclone backup to cloud storage                                         
+rclone sync $DESDIR onedrive:/Backups/LinBPQ --progress                         # Rclone backup to cloud storage, comment this line out if not required
 #
 # Job's a goodun :)
